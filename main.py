e773a6e305427fe161ab0336a74f8a0acb300e13
Petteri Vaarala <petteri.vaarala@gmail.com>
2018-01-18 18:52:03 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import ds18x20
+import machine
+import onewire
+import urequests
+import time
+
+# Read config
+#   Get essentials 
+#   Loop tags
+
+# Get data from sensors
+#   Loop sensors
+#   Get some UID
+
+# Send data to Influx
+#   REST api
